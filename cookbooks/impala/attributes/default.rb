0a51032433ea5f3be8a2f3c4dbfa903166e7a056
David Knupp <dknupp@cloudera.com>
2017-07-06 10:47:29 -0700

    try to hack up java 8 install

--- cookbooks/impala/attributes/default.rb
+++ cookbooks/impala/attributes/default.rb
@@ -6,16 +6,16 @@
 
 username = ''
 
-# User options 
+# User options
 default['impala_dev']['username'] = username
 
 # Java options
 default['java']['install_flavor'] = 'oracle'
-default['java']['jdk_version'] = '7'
+default['java']['jdk_version'] = '8'
 default['java']['oracle']['accept_oracle_download_terms'] = true
 
 # Postgres options
-case 
+case
   when (node['platform_family'] == "debian" and node['platform_version'] == "15.04")
     default['postgresql']['pgdg']['release_apt_codename'] = 'trusty'
     default['postgresql']['version']  =                     '9.4'
@@ -32,8 +32,8 @@ case
     default['postgresql']['version'] = "9.3"
 end
 
-default['postgresql']['pg_hba'] = [{:type => 'local', :db => 'all', :user => 'postgres', :addr => nil, :method => 'trust'}, 
-			           {:type => 'host', :db => 'all', :user => 'all', :addr => '127.0.0.1/32', :method => 'trust'}, 
+default['postgresql']['pg_hba'] = [{:type => 'local', :db => 'all', :user => 'postgres', :addr => nil, :method => 'trust'},
+			           {:type => 'host', :db => 'all', :user => 'all', :addr => '127.0.0.1/32', :method => 'trust'},
 				   {:type => 'host', :db => 'all', :user => 'all', :addr => '::1/128', :method => 'trust'}]
 default['postgresql']['password']['postgres'] = ''
 default['postgresql']['config']['standard_conforming_strings'] = 'off'
